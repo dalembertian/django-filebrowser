07fdc3a3c2f33bf0a6d41ff8512a9fb74934582f
Rubens Altimari <rubens@introspection.com.br>
2012-08-19 17:08:15 -0300

    Including data dirs

--- setup.py
+++ setup.py
@@ -6,11 +6,5 @@ setup(
     name = "filebrowser",
     version = "0.9-whitehat",
     packages = find_packages(),
-    
     include_package_data = True,
-
-    author = "Nelson Altimari",
-    author_email = "nelson@whitehat.com.br",
-    description = "Filebrowser fork",
-    license = "MIT License",
 )
